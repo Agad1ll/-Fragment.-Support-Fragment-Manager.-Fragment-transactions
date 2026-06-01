dde2b9aee284fc3fe0518dd677668036112f57b0
Агадил <agadilkyltaevse@gmail.com>
2026-06-01 17:54:38 +0500

    Обновил проект и перенес код

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,4 +1,4 @@
-plugins {
+    plugins {
     alias(libs.plugins.android.application)
 }
 
@@ -32,6 +32,9 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+    }
+    buildFeatures {
+        viewBinding = true
     }
 }
 
--- app/src/main/java/com/example/my_tests/MainActivity.kt
+++ app/src/main/java/com/example/my_tests/MainActivity.kt
@@ -2,19 +2,31 @@ package com.example.my_tests
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.BinderThread
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.my_tests.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btn1.setOnClickListener {
+            binding.Txt1.text="Boom!"
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
+
         }
     }
 }
