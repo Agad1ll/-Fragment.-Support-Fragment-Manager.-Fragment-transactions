22acf14618bf825b0b0893d4e5709b481e9e591c
Агадил <agadilkyltaevse@gmail.com>
2026-06-09 22:13:39 +0500

    Fragment

--- app/src/main/java/com/example/my_tests/MainActivity.kt
+++ app/src/main/java/com/example/my_tests/MainActivity.kt
@@ -42,7 +42,21 @@ class MainActivity : AppCompatActivity() {
         val adapter= ExampleRecyclerAdapter()
         binding.rcProducts.adapter=adapter
         adapter.submitList(listOfProduct)
+        val fragment1= ExampleFirstFragment()
+        binding.btnShowFragmentFirst.setOnClickListener {
+            supportFragmentManager.beginTransaction().add(binding.tvPlaceForFragment.id,fragment1).commit()
+        val fragment2= ExampleSecondFragment()
+        binding.button5.setOnClickListener {
+            supportFragmentManager.beginTransaction().replace(binding.tvPlaceForFragment.id,fragment2).commit()
+        }
 
+        binding.btnRemoveFragment.setOnClickListener {
+            supportFragmentManager.beginTransaction().remove(fragment1).commit()
+            supportFragmentManager.beginTransaction().remove(fragment2).commit()
+        }
+
+
+        }
 
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
